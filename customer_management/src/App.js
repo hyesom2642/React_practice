3bb537f3af2591e286a1d67d7e506939b7745957
hyesom2642 <kimhyesom@naver.com>
2022-02-12 13:27:07 +0900

    Material UI를 활용한 Table사용

--- customer_management/src/App.js
+++ customer_management/src/App.js
@@ -1,8 +1,18 @@
+/* eslint-disable */
 import React, { useState } from 'react';
 import './App.css';
 import Customer from './components/Customer.js';
 
+// Table import
+import Paper from '@material-ui/core/Paper';
+import Table from '@material-ui/core/Table';
+import TableHead from '@material-ui/core/TableHead';
+import TableBody from '@material-ui/core/TableBody';
+import TableRow from '@material-ui/core/TableRow';
+import TableCell from '@material-ui/core/TableCell';
+
 function App() {
+  // 고객정보
   let [customer, setCustomer] = useState([
   {
     id: 1,
@@ -28,20 +38,38 @@ function App() {
     gender: '여',
     job: '무직'
   }]);
+
   return (
     <div className="App">
-      <div className="gray-background">
+      <div className="background">
         <h2> Customer Management System </h2>
         {/* <Customer customer={ customer[0] } />
         <Customer customer={ customer[1] } />
         <Customer customer={ customer[2] } /> */}
-        {
+        {/* {
           customer.map( (v) => {
             return(
-              <Customer customer={ v }/>
+              <Customer key={customer.id} customer={ v }/>
             )
           })
-        }
+        } */}
+        <Paper className="root">
+          <Table className="table">
+            <TableHead>
+              <TableRow>
+                <TableCell>번호</TableCell>
+                <TableCell>사진</TableCell>
+                <TableCell>이름</TableCell>
+                <TableCell>나이</TableCell>
+                <TableCell>성별</TableCell>
+                <TableCell>직업</TableCell>
+              </TableRow>
+            </TableHead>
+            <TableBody>
+                { customer.map( (v) => { return( <Customer key={customer.id} customer={ v } />) }) }
+            </TableBody>
+          </Table>
+        </Paper>
       </div>
     </div>
   );
--- customer_management/src/components/Customer.js
+++ customer_management/src/components/Customer.js
@@ -1,39 +1,48 @@
 import React from 'react';
+import TableRow from '@material-ui/core/TableRow';
+import TableCell from '@material-ui/core/TableCell';
 
 function Customer(props){
     return(
-        <div className="Customer-container">
-            <CustomerProfile
-            image={ props.customer.image }
-            id={ props.customer.id }
-            name={ props.customer.name }
+        <>
+            <TableRow>
+                <TableCell>{ props.customer.id }</TableCell>
+                <TableCell><img src={ props.customer.image } /></TableCell>
+                <TableCell>{ props.customer.name }</TableCell>
+                <TableCell>{ props.customer.age }</TableCell>
+                <TableCell>{ props.customer.gender }</TableCell>
+                <TableCell>{ props.customer.job }</TableCell>
+            </TableRow>
+            {/* <CustomerProfile
+            image = { props.customer.image }
+            name = { props.customer.name }
             />
             <CustomerInfo 
-            age={ props.customer.age }
-            gender={ props.customer.gender }
-            job={ props.customer.job }
-            />
-        </div>
+            age = { props.customer.age }
+            gender = { props.customer.gender }
+            job = { props.customer.job }
+            /> */}
+        </>
     );
 };
 
-function CustomerProfile(props){
-    return(
-        <div className="CustomerProfile-conatiner">
-            <img src={ props.image } alt="profile" />
-            <h3> { props.id }.</h3>
-            <h3> { props.name }</h3>
-        </div>
-    );
-}
-function CustomerInfo(props){
-    return(
-        <div className="CustomerInfo-container">
-            <p>{ props.age }</p>
-            <p>{ props.gender }</p>
-            <p>{ props.job }</p>
-        </div>
-    );
-}
+// function CustomerProfile(props){
+//     return(
+//         <div className="CustomerProfile-conatiner">
+//             <img src={ props.image } alt="profile" />
+//             <h3> { props.id }.</h3>
+//             <h3> { props.name }</h3>
+//         </div>
+//     );
+// }
+// function CustomerInfo(props){
+//     return(
+//         <div className="CustomerInfo-container">
+//             <p>{ props.age }</p>
+//             <p>{ props.gender }</p>
+//             <p>{ props.job }</p>
+//         </div>
+//     );
+// }
 
 export default Customer;
